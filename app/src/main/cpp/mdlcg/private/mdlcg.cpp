f21f548108653ebf27788a8ef9ed54bb258d9637
ZhengChengkai826 <zchengkai826@gmail.com>
2019-06-20 12:31:04 +0800

    c

---
+++ app/src/main/cpp/mdlcg/private/mdlcg.cpp
@@ -0,0 +1,6 @@
+//
+// Created by andys on 6/20/2019.
+//
+#include <mdlcg.h>
+
+#include <CGAL/Simple_cartesian.h>
---
+++ app/src/main/cpp/mdlcg/public/mdlcg.h
@@ -0,0 +1,8 @@
+//
+// Created by andys on 6/20/2019.
+//
+
+#ifndef MYDREAMLAND_MDLCG_H
+#define MYDREAMLAND_MDLCG_H
+
+#endif //MYDREAMLAND_MDLCG_H
